50e245053a5ff92ba776238357162f01e464c0fa
Anthony Gouton <anthony.gouton.wcs@gmail.com>
2022-04-06 17:26:22 +0200

    result challenge

--- public/index.php
+++ public/index.php
@@ -5,5 +5,15 @@ require_once __DIR__ . '/../config/twig.php';
 
 
 $name = 'Wilder';
+$persons = [
+    ['name' => 'riri', 'age' => 10],
+    ['name' => 'fifi', 'age' => 20],
+    ['name' => 'loulou', 'age' => 30],
+ ];
 
-echo $twig->render('home.html.twig', ['name' => $name]);
+ 
+
+//echo $twig->render('home.html.twig', ['name' => $name]);
+//echo $twig->render('hello.html.twig', ['name' => $name]);
+echo $twig->render('person.html.twig', ['persons' => $persons]);
+
